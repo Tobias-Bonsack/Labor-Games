5c7dd44bd154fc93dde05c4879147f7e41d939d9
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-10-03 12:30:54 +0200

    LG-4 #done
    create 2d blend tree for movement

--- Assets/My Packages/Third-Person Controller/Scripts/PlayerMovement.cs
+++ Assets/My Packages/Third-Person Controller/Scripts/PlayerMovement.cs
@@ -7,9 +7,10 @@ namespace ThirdPersonController
 {
     public class PlayerMovement : MonoBehaviour
     {
-
+        #region Parameters
         private CharacterController _controller;
         [SerializeField] Transform _mainCamera;
+        [SerializeField] Animator _animator;
 
         [Header("Jump")]
         [SerializeField] float _jumpHeight;
@@ -25,12 +26,20 @@ namespace ThirdPersonController
         [SerializeField] float _turnTime;
         private float _turnSmoothVelocity;
         private Vector3 _moveDir;
+        private float _targetAngle = 0f;
 
         [Header("Gravity")]
         private float _gravity = -9.81f;
         private Vector3 _velocity = Vector3.zero;
         private float _basicDown = -1f;
 
+        #region Animation Parameter
+        private Vector3 _oldVelocity = Vector3.zero;
+        private float _oldAngle = 0f;
+        #endregion
+        #endregion
+
+        #region Unity Events
         void Awake()
         {
             _controller = GetComponent<CharacterController>();
@@ -38,26 +47,34 @@ namespace ThirdPersonController
 
         private void FixedUpdate()
         {
+            _oldVelocity = _velocity;
+            _oldAngle = transform.rotation.eulerAngles.y;
+
             CalculateMovement();
             CalculateResistance();
             CalculateGravity();
 
-            //TODO here place for extern forces
+            //TODO here place for extern forces, maybe as an list of extern calls
 
+            UpdateAnimation();
             _controller.Move(_velocity * Time.deltaTime);
 
         }
+        #endregion
+
+        #region FixedUpdate Methods
         private void CalculateMovement()
         {
             if (_isMoving && _controller.isGrounded)
             {
-                float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnTime);
+                _targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetAngle, ref _turnSmoothVelocity, _turnTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-                _moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                _moveDir = Quaternion.Euler(0f, _targetAngle, 0f) * Vector3.forward;
                 AddForce(_moveDir * _acceleration * Time.deltaTime, false);
 
+                //TODO could be troublesome wiht extern force maybe, maybe not
                 _velocity.x = Mathf.Clamp(_velocity.x, -_maxSpeed, _maxSpeed);
                 _velocity.z = Mathf.Clamp(_velocity.z, -_maxSpeed, _maxSpeed);
             }
@@ -96,6 +113,12 @@ namespace ThirdPersonController
             else if (_velocity.y < _basicDown) { _velocity.y = _basicDown; }
         }
 
+        private void UpdateAnimation()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
 
         public void Jump()
         {
@@ -110,6 +133,7 @@ namespace ThirdPersonController
         public void AddForce(Vector3 force, bool reset) => _velocity = reset ? force : force + _velocity;
 
         private int OppositeSign(float number) => number > 0f ? -1 : 1;
+
         //TODO maybe delete later, dont know if needed at any time
         private bool SameSign(float num1, float num2) => Mathf.Sign(num1) == Mathf.Sign(num2);
     }
