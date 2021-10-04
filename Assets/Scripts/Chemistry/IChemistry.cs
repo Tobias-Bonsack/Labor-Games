44272a5dfd409a94e2a6d6ce40c4149b3b0bb447
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-10-04 17:44:34 +0200

    LG-3 #time 1h
    start with Chemistry-Engine

---
+++ Assets/Scripts/Chemistry/IChemistry.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ChemistryEngine
+{
+    public interface IChemistry
+    {
+        public enum ChemistryTypes
+        {
+            HEAT,
+            COLD,
+            ELECTRICITY
+        }
+    }
+
+}
