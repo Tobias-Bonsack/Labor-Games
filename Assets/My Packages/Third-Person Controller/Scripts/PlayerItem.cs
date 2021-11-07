baee20937e1e25b2a580b3487db5b4c8395901d8
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-11-07 11:33:28 +0100

    snowball implements

--- Assets/My Packages/Third-Person Controller/Scripts/PlayerItem.cs
+++ Assets/My Packages/Third-Person Controller/Scripts/PlayerItem.cs
@@ -36,7 +36,6 @@ namespace ThirdPersonController
                 _holdItem = hitInfo.collider.gameObject;
                 _holdItem.transform.SetParent(_holdPlace.transform);
                 _holdItem.transform.localPosition = Vector3.zero;
-                _holdItem.transform.localScale = Vector3.one;
                 _holdItem.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
 
                 _holdItem.GetComponent<Collider>().enabled = false;
