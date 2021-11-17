918b93319c5010b591f113c63d9cddd77fce02ba
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-11-17 10:29:07 +0100

    electro button implemented
    transform changer

--- Assets/Scripts/Chemistry/General Properties/TransformChange.cs
+++ Assets/Scripts/Chemistry/General Properties/TransformChange.cs
@@ -35,6 +35,8 @@ namespace ChemistryEngine
 
         private void ChangeScale(object sender, EventArgs e)
         {
+            Debug.Log("Position: " + (_originPosition + _addPosition * _elementReceiver._elementPercent));
+            Debug.Log("Rotation: " + (_originRotation + _addRotation * _elementReceiver._elementPercent));
             if (_changePosition) _toChange.position = _originPosition + _addPosition * _elementReceiver._elementPercent;
             if (_changeRotation) _toChange.rotation = Quaternion.Euler(_originRotation + _addRotation * _elementReceiver._elementPercent);
             if (_changeScale) _toChange.localScale = _originScale + _addScale * _elementReceiver._elementPercent;
