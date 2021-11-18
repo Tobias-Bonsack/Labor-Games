0a2011b1300be4325367fa84b2a9035a2fd8e0ba
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-11-18 11:30:39 +0100

    transform change can be stacked

--- Assets/Scripts/Chemistry/General Properties/TransformChange.cs
+++ Assets/Scripts/Chemistry/General Properties/TransformChange.cs
@@ -9,6 +9,7 @@ namespace ChemistryEngine
     {
         [SerializeField] Transform _toChange;
         [SerializeField, Range(0f, 1f)] float _pointToStartChagne = 0f, _pointToEndChange = 1f;
+        private bool _knowOrigin = false;
 
         [Header("Position")]
         [SerializeField] bool _changePosition;
@@ -36,11 +37,23 @@ namespace ChemistryEngine
 
         private void ChangeScale(object sender, EventArgs e)
         {
+            if (!_knowOrigin && _elementReceiver.ElementPercent >= _pointToStartChagne)
+            {
+                _knowOrigin = true;
+                _originPosition = _toChange.position;
+                _originRotation = _toChange.rotation.eulerAngles;
+                _originScale = _toChange.localScale;
+            }
+            
             float range = _pointToEndChange - _pointToStartChagne;
-            float changePercent = Mathf.Clamp(_elementReceiver.ElementPercent - _pointToStartChagne, 0f, range) / range;
-            if (_changePosition) _toChange.position = _originPosition + _addPosition * changePercent;
-            if (_changeRotation) _toChange.rotation = Quaternion.Euler(_originRotation + _addRotation * changePercent);
-            if (_changeScale) _toChange.localScale = _originScale + _addScale * changePercent;
+            float fromStart = _elementReceiver.ElementPercent - _pointToStartChagne;
+            if (fromStart < range)
+            {
+                float changePercent = Mathf.Clamp(fromStart, 0f, range) / range;
+                if (_changePosition) _toChange.position = _originPosition + _addPosition * changePercent;
+                if (_changeRotation) _toChange.rotation = Quaternion.Euler(_originRotation + _addRotation * changePercent);
+                if (_changeScale) _toChange.localScale = _originScale + _addScale * changePercent;
+            }
         }
     }
 }
